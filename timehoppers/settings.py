ad85f15efcd2e5b61410346217726243c2638135
ciaraosull <ciaraosull@gmail.com>
2022-07-30 19:51:48 +0000

    Add media settings & test with temp image

--- timehoppers/settings.py
+++ timehoppers/settings.py
@@ -156,9 +156,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # may need to take out when setting up AWS
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
