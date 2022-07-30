324042dd7fc59938ba2e1136d68d5101415b17fd
ciaraosull <ciaraosull@gmail.com>
2022-07-30 13:41:11 +0000

    Add home class-based view and link up urls to view home page

---
+++ home/views.py
@@ -0,0 +1,10 @@
+"""
+Imports for Home Views
+"""
+
+from django.views.generic import TemplateView
+
+
+class HomeView(TemplateView):
+    """ Class-based view to return home page """
+    template_name = 'home/index.html'
