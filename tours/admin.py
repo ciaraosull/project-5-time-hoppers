f7b3df1702042785ceadccfcfb99fcdfcb12dfce
ciaraosull <ciaraosull@gmail.com>
2022-07-31 14:20:32 +0000

    Fix line too long error

--- tours/admin.py
+++ tours/admin.py
@@ -7,7 +7,14 @@ from .models import Tour, Category, Review
 @admin.register(Tour)
 class TourAdmin(SummernoteModelAdmin):
     """ Display Tour Model on Admin Site """
-    list_display = ('date_added', 'tour_name', 'category', 'rating', 'price', 'image')
+    list_display = (
+        'date_added',
+        'tour_name',
+        'category',
+        'rating',
+        'price',
+        'image'
+    )
     search_fields = ['tour_name', 'description']
     list_filter = ('date_added', 'price', 'rating')
     summernote_fields = ('description')
