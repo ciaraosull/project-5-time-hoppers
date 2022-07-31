b1ef8fb19d3533e8a07b98d220fb8dbfd0d87886
ciaraosull <ciaraosull@gmail.com>
2022-07-31 14:00:06 +0000

    Alter Tour & Category models & add review model

--- tours/models.py
+++ tours/models.py
@@ -1,11 +1,16 @@
 """ Imports for Tours """
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
     """ Categorise for User to Filter Tours"""
-    name = models.CharField(max_length=250)
+    category_name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250, null=True, blank=True)
+
+    class Meta:
+        """ To display the plural"""
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         """ To return the name objects as a string """
@@ -27,8 +32,24 @@ class Tour(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    tour_date = models.DateField()
 
     def __str__(self):
         """ To return the name objects as a string """
         return self.tour_name
+
+
+class Review(models.Model):
+    """ Model for Comments """
+    tour = models.ForeignKey(
+        Tour, on_delete=models.CASCADE, related_name='reviews')
+    name = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='commenter')
+    your_review = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        """ Ordering reviews by first created """
+        ordering = ["date_posted"]
+
+    def __str__(self):
+        return f"Review {self.your_review} by {self.name}"
