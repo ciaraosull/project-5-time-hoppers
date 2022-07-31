1049b7abdf6e9a7d4d6deb3aa201074856cfd1ee
ciaraosull <ciaraosull@gmail.com>
2022-07-31 14:31:07 +0000

    Create ListView to display all tours

--- tours/views.py
+++ tours/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""
+Imports for Tours View
+"""
+from django.views.generic import ListView
+from .models import Tour
 
-# Create your views here.
+
+class TourListView(ListView):
+    """ A view to show all tours including sort & search """
+    model = Tour
+    template_name = 'tours/tours.html'
+    context_object_name = 'tours'
+    ordering = ['-date_added']
+    paginate_by = 6
