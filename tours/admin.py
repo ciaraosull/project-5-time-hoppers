c3d8ec61d941b86035e5ad34ec5e64e18b92879e
ciaraosull <ciaraosull@gmail.com>
2022-07-31 10:43:37 +0000

    Register Category model to admin site & manual test

--- tours/admin.py
+++ tours/admin.py
@@ -1,7 +1,7 @@
 """ Imports for admin site"""
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Tour
+from .models import Tour, Category
 
 
 @admin.register(Tour)
@@ -11,3 +11,6 @@ class PostAdmin(SummernoteModelAdmin):
     search_fields = ['tour_name', 'description']
     list_filter = ('price', 'rating')
     summernote_fields = ('description')
+
+
+admin.site.register(Category)
