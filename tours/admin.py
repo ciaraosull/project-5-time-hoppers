290d7939973e4b0728e07aad34161ce8a5ff3f97
ciaraosull <ciaraosull@gmail.com>
2022-08-26 18:10:46 +0000

    Register booking & departure models to admin site

--- tours/admin.py
+++ tours/admin.py
@@ -1,7 +1,7 @@
 """ Imports for admin site"""
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Tour, Category, Review
+from .models import Tour, Category, Review, DepartureTime, Booking
 
 
 @admin.register(Tour)
@@ -11,6 +11,7 @@ class TourAdmin(SummernoteModelAdmin):
         'date_added',
         'tour_name',
         'category',
+        'tour_duration'
         'rating',
         'price',
         'image'
@@ -32,3 +33,23 @@ class ReviewAdmin(admin.ModelAdmin):
     list_display = ('name', 'your_review', 'tour', 'date_posted')
     list_filter = ('date_posted', 'name')
     search_fields = ('name', 'your_review')
+
+
+@admin.register(DepartureTime)
+class DepartureTimeAdmin(admin.ModelAdmin):
+    """Class to display reviews on admin site"""
+    list_display = ('departure_time')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """ Display Booking Model on Admin Site """
+    list_display = (
+        'tour_name',
+        'book_tour_date',
+        'departure_time',
+        'quantity'
+        'date_added'
+    )
+    search_fields = ['tour_name']
+    list_filter = ('date_added', 'quantity', 'book_tour_date')
