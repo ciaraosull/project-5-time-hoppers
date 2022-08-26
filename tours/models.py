cc9be57f9d4b9f2be7de16f119726071200685f3
ciaraosull <ciaraosull@gmail.com>
2022-08-26 18:48:51 +0000

    Remove validator & decimal imports as small interger field used instead of decimal

--- tours/models.py
+++ tours/models.py
@@ -1,8 +1,6 @@
 """ Imports for Tours """
-from decimal import Decimal
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
