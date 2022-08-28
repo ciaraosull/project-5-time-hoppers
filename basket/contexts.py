55412673c0e1e8547c8f75c21fa11ece613f382a
ciaraosull <ciaraosull@gmail.com>
2022-08-28 21:30:59 +0000

    Create shopping basket contexts

---
+++ basket/contexts.py
@@ -0,0 +1,19 @@
+""" Shopping Basket Contexts """
+
+
+def basket_contents(request):
+    """
+    To track bookings in basket & return context aross all views
+    """
+
+    basket_items = []
+    total = 0
+    product_count = 0
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count,
+    }
+
+    return context
