0416867b767bbab592e1c9e37e9a05720ccffeec
ciaraosull <ciaraosull@gmail.com>
2022-09-03 16:11:50 +0000

    Delete old bookng form and uneeded files and manual test new add to basket function

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,4 +1,4 @@
-"""Context Processor"""
+""" Context Processor"""
 from django.shortcuts import get_object_or_404
 from tours.models import Tour
 
@@ -11,20 +11,20 @@ def basket_contents(request):
     tour_count = 0
     basket = request.session.get('basket', {})
 
-    for item_id, item_data in basket.items():
+    for item_id, quantity in basket.items():
         tour = get_object_or_404(Tour, pk=item_id)
-        tour += item_data * tour.price
-        tour_count += item_data
+        total += quantity * tour.price
+        tour_count += quantity
         basket_items.append({
             'item_id': item_id,
-            'quantity': item_data,
+            'quantity': quantity,
             'tour': tour,
         })
 
     context = {
         'basket_items': basket_items,
         'total': total,
-        'tour_count': tour_count
+        'tour_count': tour_count,
     }
 
     return context
--- basket/views.py
+++ basket/views.py
@@ -13,22 +13,12 @@ def add_to_basket(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    departure_time = request.POST['tour_departure_time']
     basket = request.session.get('basket', {})
 
-    if departure_time:
-        if item_id in list(basket.keys()):
-            if departure_time in basket[item_id]['items_by_departure_time'].keys():
-                basket[item_id]['items_by_departure_time'][departure_time] += quantity
-            else:
-                basket[item_id]['items_by_departure_time'][departure_time] = quantity
-        else:
-            basket[item_id] = {'items_by_departure_time': {departure_time: quantity}}
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
     else:
-        if item_id in list(basket.keys()):
-            basket[item_id] += quantity
-        else:
-            basket[item_id] = quantity
+        basket[item_id] = quantity
 
     request.session['basket'] = basket
     return redirect(redirect_url)
