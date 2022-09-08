d0798485ee9bc1f81090f0126d71ecd782b74b5b
ciaraosull <ciaraosull@gmail.com>
2022-09-08 20:23:17 +0000

    Create cutomised form for checkout, add signal & create checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+"""Views for Checkout Page"""
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def checkout(request):
+    """To create a view for the checkout &
+    redirect users if checkout typed into url
+    """
+    basket = request.session.get('bag', {})
+    if not basket:
+        messages.error(request, "Your Basket is Empty")
+        return redirect(reverse('tours-list'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
