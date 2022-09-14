90bece44ea0264aa423c0740726159dadca76150
ciaraosull <ciaraosull@gmail.com>
2022-09-14 21:53:01 +0000

    Complete webhook handler & test

--- checkout/views.py
+++ checkout/views.py
@@ -55,7 +55,13 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            # prevent multiple save events on db
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(basket)
+            order.save()
+
             for item_id, item_data in basket.items():
                 try:
                     tour = Tour.objects.get(id=item_id)
