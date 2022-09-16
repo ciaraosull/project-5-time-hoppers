5cc72952c1be27fba40049ab733c0dfe977dcd1c
ciaraosull <ciaraosull@gmail.com>
2022-09-16 16:40:21 +0000

    Populate profile page with user details and add update functionality

--- profiles/forms.py
+++ profiles/forms.py
@@ -21,4 +21,31 @@ class ProfileUpdateForm(forms.ModelForm):
         Get profile model and choose fields to display
         """
         model = Profile
-        fields = ['profile_image']
+        fields = ('profile_image', 'default_phone_number',
+                  'default_street_address1', 'default_street_address2',
+                  'default_town_or_city', 'default_postcode',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_postcode': 'Postal Code',
+            'default_town_or_city': 'Town or City',
+            'default_street_address1': 'Street Address 1',
+            'default_street_address2': 'Street Address 2',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'profile_image':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'border-black'
+            self.fields[field].label = False
