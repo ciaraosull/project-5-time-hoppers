682b68643988d1f85ec77cdf427c212ec846bfd7
ciaraosull <ciaraosull@gmail.com>
2022-09-23 21:45:48 +0000

    Change background image displayed depending on time of day

--- static/js/landing_page.js
+++ static/js/landing_page.js
@@ -11,7 +11,7 @@ const showAmPm = true;
 
 // Show Time & Update every second
 function showTime() {
-    let today = new Date(), hour = today.getHours(), min = today.getMinutes(), sec = today.getSeconds();
+    let today = new Date(), hour = today.getHours(), mins = today.getMinutes(), secs = today.getSeconds();
 
     // Set AM or PM on Clock depending on condition of hour
     const amPm = hour >= 12 ? 'PM' : 'AM';
@@ -20,10 +20,43 @@ function showTime() {
     hour = hour % 12 || 12;
 
     // Output Time
-    time.innerHTML = `${hour}<span>:</span>${min}<span>:</span>${sec}`;
+    time.innerHTML = `${hour}<span>:</span>${addZero(mins)}<span>:</span>${addZero(
+        secs
+    )} ${showAmPm ? amPm : ''}`;
 
     setTimeout(showTime, 1000);
 }
 
+// Add Zeros to 1 digit numbers in clock
+function addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+}
+
+// Set Background
+function setBgImage() {
+    let today = new Date(),
+      hour = today.getHours();
+  
+    if (hour < 6) {
+      // Morning
+      document.body.style.backgroundImage = "url('https://res.cloudinary.com/ciara0s1980/image/upload/v1663962553/image1_wqtpf3.png')";
+
+    } else if (hour < 12) {
+        // Afternoon
+        document.body.style.backgroundImage = "url('https://res.cloudinary.com/ciara0s1980/image/upload/v1663962562/image5_nr0pjn.png')";
+
+    } else if (hour < 18) {
+      // Afternoon
+      document.body.style.backgroundImage = "url('https://res.cloudinary.com/ciara0s1980/image/upload/v1663962555/image2_x0y8yu.png')";
+
+    } else {
+      // Evening
+      document.body.style.backgroundImage = "url('https://res.cloudinary.com/ciara0s1980/image/upload/v1663962553/image6_j8i8lf.png')";
+      
+      document.body.style.color = 'white';
+    }
+  }
+
 // Run
 showTime();
+setBgImage();
