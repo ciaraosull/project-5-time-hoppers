68f022e9430e697c2e315590bf98b86f95945751
ciaraosull <ciaraosull@gmail.com>
2022-09-24 19:07:46 +0000

    Email newsletter to terminal for testing

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,6 +1,8 @@
 """Views for Subscibers form & Newsletter"""
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail
 from .forms import SubsciberForm, NewsletterForm
 
 
@@ -22,7 +24,23 @@ def subscribe(request):
 
 def newsletter(request):
     """Newsletter Form View"""
-    form = NewsletterForm()
+
+    if request.method == 'POST':
+        form = NewsletterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            send_mail(
+                'Test Title',
+                'Test Body',
+                settings.DEFAULT_FROM_EMAIL,
+                ['ciaraosull@gmail.com'],
+                fail_silently=False,  # if email saved is incorrect
+            )
+            messages.success(request, 'Newsletter Sent Successfully')
+            return redirect('newsletter')
+    else:
+        form = NewsletterForm()
+
     context = {
         'form': form,
     }
