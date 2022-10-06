92cae4ded6dae6ecc8b9af292e004e518b50c967
ciaraosull <ciaraosull@gmail.com>
2022-10-06 20:47:32 +0000

    Add Privacy Policy view, link in footer & content

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 """ URLS for Home App"""
 from django.urls import path
-from home.views import HomeView
+from home.views import HomeView, PrivacyView, AccessibilityView
 
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('privacy/', PrivacyView.as_view(), name='privacy'),
+    path('accessibility/', AccessibilityView.as_view(), name='accessibility'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,13 @@ from django.views.generic import TemplateView
 class HomeView(TemplateView):
     """ Class-based view to return home page """
     template_name = 'home/index.html'
+
+
+class PrivacyView(TemplateView):
+    """ Class-based view to return home page """
+    template_name = 'home/privacy_statement.html'
+
+
+class AccessibilityView(TemplateView):
+    """ Class-based view to return home page """
+    template_name = 'home/accessibility.html'
